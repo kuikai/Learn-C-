3eafc7880e46a3635250844cedffac674c4476d6
kuikai <mikkeldixen@live.dk>
2018-09-12 19:08:26 +0200

    m,m

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kage;
+            int age;
+            int age1;
+            int reau;
+            Console.WriteLine("halloo press a key");
+            Console.Read();
+
+            Console.Write("Your name:");
+            kage = Convert.ToString(Console.ReadLine());
+            
+            Console.Write("your age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("your age:");
+            
+            Console.WriteLine(kage+);
+            Console.ReadKey();
+            
+           
+        }
+    }
+}
